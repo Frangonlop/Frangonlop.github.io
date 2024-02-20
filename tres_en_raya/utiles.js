114131f58b3f0cd58b7605ce4910c667aeeaf340
Francisco González <frangonlop004@gmail.com>
2024-02-20 12:37:57 +0100

    Create utiles.js

---
+++ tres_en_raya/utiles.js
@@ -0,0 +1,15 @@
+function Suma(a, b) {
+  return a + b;
+}
+
+function Resta(a, b) {
+  return a - b;
+}
+
+function Swiper(a, b) {
+  return a - b;
+}
+
+export { Suma, Resta, Swiper };
+
+// export default Suma;
